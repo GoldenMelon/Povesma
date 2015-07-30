2f81f20b3dcf1a743ef8fd163b5e86583e43faea
GoldenMelon <goldenmelon401@gmail.com>
2015-07-30 17:42:23 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Povesma::Application.config.secret_key_base = 'f9cc1ee3763705a45913f2515d0997e54073cf766b275d1035d474e5074b02bfb779c49aafe5c430dd5297940907fe1eddbd44ec283e1d537c99a0d180c134b5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Povesma::Application.config.session_store :cookie_store, key: '_povesma_session'
